25dc867ec45cb6e0572487386537be4118d147b6
Wojtek <63168725+xxFroggyxx@users.noreply.github.com>
2024-11-25 20:33:31 +0100

    change hero content

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,44 +1,17 @@
-import NextLogo from "./next-logo";
-import SupabaseLogo from "./supabase-logo";
+import { SignUpDialog } from "@/components/signup-dialog";
 
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
-      </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
+    <div className="flex flex-col items-center gap-16">
+      <h1 className="sr-only">
+        Aplikacja webowa do zarządzania zawodami wędkarskimi.
+      </h1>
+      <p className="mx-auto max-w-xl text-center text-3xl !leading-tight lg:text-4xl">
+        <span className="font-bold">Dołącz do zawodów wędkarskich</span> -{" "}
+        <span className="font-thin">zarejestruj się już </span>
+        <span className="font-bold uppercase underline">dziś!</span>
       </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+      <SignUpDialog />
     </div>
   );
 }
