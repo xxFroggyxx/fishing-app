7a2d976dab2cf54bb3b7aeeef442195f11b93095
Wojtek <63168725+xxFroggyxx@users.noreply.github.com>
2024-12-02 20:35:02 +0100

    create organization signup form

--- app/(auth-pages)/sign-up/organization/page.tsx
+++ app/(auth-pages)/sign-up/organization/page.tsx
@@ -1,5 +1,133 @@
-import React from 'react';
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(20),
+  organizationName: z.string(),
+  address: z.string(),
+  phone: z.string(),
+});
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      organizationName: "",
+      address: "",
+      phone: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    console.log(values);
+  }
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <h1 className="text-2xl font-medium">
+            Zarejestruj się - organizator
+          </h1>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="mail@przyklad.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Hasło</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Hasło" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="organizationName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nazwa organizacji</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Organizacja" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Adres</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Słoneczne 2A/15, Stargard, 73-110"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Telefon</FormLabel>
+                <FormControl>
+                  <Input type="tel" placeholder="123 456 789" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Załóż konto</Button>
+        </form>
+      </Form>
+    </>
+  );
 }
