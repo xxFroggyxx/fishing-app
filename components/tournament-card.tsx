377babc92e5f8bf3493d08f34bb40ed1d4b4dc2e
Wojtek <63168725+xxFroggyxx@users.noreply.github.com>
2024-12-08 19:09:09 +0100

    color fix on tournament-card

--- components/tournament-card.tsx
+++ components/tournament-card.tsx
@@ -17,17 +17,15 @@ export default function TournamentCard({
 }: TournamentCardProps) {
   return (
     <Link href="/">
-      <div className="max-w-sm rounded-lg border border-b-foreground/10 bg-white shadow-md dark:bg-background">
+      <div className="max-w-sm rounded-lg border border-b-foreground/10 shadow-md">
         <img
           className="max-h-64 rounded-t-lg"
           src={`${imageOptions.src}`}
           alt={`${imageOptions.alt}`}
         />
         <div className="p-5">
-          <h3 className="mb-2 text-2xl font-bold tracking-tight dark:text-white">
-            {title}
-          </h3>
-          <p className="mb-3 whitespace-pre-line font-light dark:text-foreground/60">
+          <h3 className="mb-2 text-2xl font-bold tracking-tight">{title}</h3>
+          <p className="mb-3 whitespace-pre-line font-light text-foreground/70">
             {description}
           </p>
         </div>
