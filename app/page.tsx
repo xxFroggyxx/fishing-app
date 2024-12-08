b9d36a053abf6c1f8e379dc05efe4fee34860c70
Wojtek <63168725+xxFroggyxx@users.noreply.github.com>
2024-12-08 19:37:19 +0100

    add all main components to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Hero from '@/components/hero';
+import Hero from "@/components/hero";
+import CurrentTournaments from "@/components/current-tournaments";
+import WhatToFish from "@/components/what-to-fish";
+import LatestBlogPosts from "@/components/latest-blog-posts";
+import OurPartners from "@/components/our-partners";
+import WhyUs from "@/components/why-us";
 
 export default async function Index() {
   return (
     <>
       <Hero />
+      <CurrentTournaments />
+      <WhatToFish />
+      <LatestBlogPosts />
+      <OurPartners />
+      <WhyUs />
     </>
   );
 }
