2d8f10f1abdd632dff549465f691d8936c47ebe3
Wojtek <63168725+xxFroggyxx@users.noreply.github.com>
2024-12-08 19:25:58 +0100

    fix color on footer-section

--- components/footer-section.tsx
+++ components/footer-section.tsx
@@ -7,7 +7,7 @@ interface FooterSectionProps {
 function FooterSection({ title, children, className }: FooterSectionProps) {
   return (
     <div className={`${className}`}>
-      <h3 className="mb-4 text-white/60">{title}</h3>
+      <h3 className="mb-4 text-foreground/70">{title}</h3>
       {children}
     </div>
   );
